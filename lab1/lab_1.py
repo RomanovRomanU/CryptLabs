10c4090095faba6a06e1004b6b7d54ead45d5ab1
Roman Romanov <Roman Romanov>
2017-03-03 01:08:03 +0200

    first part of lab1

---
+++ lab1/lab_1.py
@@ -0,0 +1,72 @@
+import re
+from collections import OrderedDict
+from math import log
+def load_text(name):
+	text = open(name,'r')
+	return text.read()
+
+def clear_text(string):
+	#substituting bad symbols
+	string = string.lower()
+	string = re.sub(r"\.|\"|\(|\)|\?|\'|\d|\t|\<|\>|\*|\\"," ",string)
+	string = re.sub(r"[qwertyuiopasdfghjklzxcvbnm:!,-;=^]"," ",string)
+	string = re.sub(r"\n"," ",string)
+	string = re.sub(r" +"," ",string)
+	string.replace('ъ','ь')
+	string.replace('ё','е')
+	return string
+
+def number_of_letters(string):
+	result = {}
+	for i in range(len(string)):
+		if string[i] in result:
+			result[string[i]]+=1
+		else:
+			result.update({string[i]:1})
+	sorted_result = OrderedDict(sorted(result.items()))
+	# return list(sorted_result.items())
+	return result
+
+def number_of_bigrams(string):
+	result = {}
+	#Вот тут можем поменять шаг,что бы 
+	#брать непересекающиеся биграммы
+	for i in range(len(string)):
+		if string[i:i+2] in result:
+			result[string[i:i+2]]+=1
+		else:
+			result.update({string[i:i+2]:1})	
+	sorted_result = OrderedDict(sorted(result.items()))
+	# return list(sorted_result.items())
+	return result
+
+def H1(string):
+	result = 0
+	string_len = len(string)
+	number_of_letters_result = number_of_letters(string)
+	chars = number_of_letters_result.keys()
+	for char in chars:
+		frequency = number_of_letters_result[char]/string_len
+		result += frequency*log(frequency,2)
+	return (-result)
+
+def H2(string):
+	result = 0
+	string_len = len(string)
+	number_of_bigrams_result = number_of_bigrams(string)
+	chars = number_of_bigrams_result.keys()
+	for char in chars:
+		frequency = number_of_bigrams_result[char]/string_len
+		result += frequency*log(frequency,2)
+	return (-result)/2
+
+
+
+if __name__=='__main__':
+	text = load_text('Voyna_i_mir.txt')
+	text = clear_text(text)
+	# result1 = number_of_letters(text)
+	# result2 = number_of_bigrams(text)
+	print(H1(text))
+	print(H2(text))
+
